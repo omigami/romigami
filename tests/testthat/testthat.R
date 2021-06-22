ec828e36e0a90d5eb685d34f33348e16ee7fe7c9
Pierre <pierre@datarevenue.com>
2021-06-22 18:28:53 +0100

    commit tests TODO : don't expose user token

---
+++ tests/testthat/testthat.R
@@ -0,0 +1,27 @@
+library(testthat)
+source("/Users/pierre/PycharmProjects/romigami/R/romigami.R")
+
+test_that("integration is correct", {
+  initialize_environment()
+  mgf_path <- paste0(dirname(getwd()), "/romigami/tests/assets/GNPS-COLLECTIONS-MISC.mgf")
+  results <- match_spectra_from_path("8DmWwNrFB8oSQcjHsEabHoNcUy30MZLY",
+                                     mgf_path,
+                                     10,
+                                     list("Smiles", "Compound_name"),
+                                     "positive"
+  )
+  expect_equal(length(results), 46)
+})
+
+test_that("token is invalid", {
+  initialize_environment()
+  mgf_path <- paste0(dirname(getwd()), "/romigami/tests/assets/GNPS-COLLECTIONS-MISC.mgf")
+  expect_error(
+    match_spectra_from_path(token = "Random_token",
+                            mgf_path = mgf_path,
+                            n_best = 10,
+                            include_metadata = list("Smiles", "Compound_name"),
+                            ion_mode = "positive"),
+    "InvalidCredentials: Your credentials are invalid, please revise your API token."
+  )
+})
