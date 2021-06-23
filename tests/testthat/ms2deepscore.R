0e971dc2ed1df6a11bea9029db43459b365e4b8f
dianacarvalho1 <diana@datarevenue.com>
2021-06-23 09:32:31 -0500

    Add tests for ms2deepscore
    Add test asset of two equal spectra
    Rename former test file to spec2vec.R
    
    --- don't change below this line ---
    MLOPS-306 #time 6m

---
+++ tests/testthat/ms2deepscore.R
@@ -0,0 +1,20 @@
+library(testthat)
+library(romigami)
+
+test_that("integration is correct", {
+  initialize_environment()
+  mgf_path <- paste0(dirname(getwd()), "/romigami/tests/assets/GNPS-2-EQUAL-SPECTRA.mgf")
+  results <- predict_similarity_of_pair(token = "8DmWwNrFB8oSQcjHsEabHoNcUy30MZLY",
+                                        mgf_path = mgf_path)
+  expect_equal(results["Tanimoto Score"], 1)
+})
+
+test_that("token is invalid", {
+  initialize_environment()
+  mgf_path <- paste0(dirname(getwd()), "/romigami/tests/assets/GNPS-2-SPECTRA.mgf")
+  expect_error(
+    predict_similarity_of_pair(token = "Random_token",
+                               mgf_path = mgf_path),
+    "InvalidCredentials: Your credentials are invalid, please revise your API token."
+  )
+})
