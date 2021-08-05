e3f270d3e63db4a6a9ac39c4af00581abd5e3cc8
Rouven <rouven@datarevenue.com>
2021-08-05 11:57:28 +0200

    Added Plotting functions to notebook and R script for now. Cant test because plotting is not yet available

--- R/romigami.R
+++ R/romigami.R
@@ -33,3 +33,65 @@ match_spectra_from_path <- function(token, mgf_path, n_best, include_metadata, i
 
   return(results)
 }
+
+
+#' Generate a grid image representation of the hits returned from Spec2Vec and MS2DeepScore outputs.
+#' All structures passed MUST have valid smiles or inchi representations.
+#'
+#' @param spectra_matches DataFrame resulting from either Spec2Vec or MS2DeepScore. Need to feature smiles or inchi, score and compound_name as columns.
+#' @param representation The representation of the molecules found in the provided dataframe
+#' @param draw_indices If true draws the indices of the atoms
+#' @param molecule_image_size The size of every individual image of a molecule. Need to be provided as a list with two ints.
+#' @param substructure_highlight Needs to be a molecule substructure represented as a smiles.
+
+#' @return A image of the molecules
+#' @export
+plot_molecule_structure_grid <- function(spectra_matches, representation, draw_indices, molecule_image_size, substructure_highlight){
+  omigami <- reticulate::import("omigami")
+
+  plotter <- omigami$MoleculePlotter()
+  results <- plotter$plot_molecule_structure_grid(spectra_matches = spectra_matches,
+                                                  representation = representation,
+                                                  draw_indices = draw_indices,
+                                                  molecule_image_size = molecule_image_size,
+                                                  substructure_highlight = substructure_highlight)
+
+  return(results)
+}
+
+
+#' Uses the ClassyFire API to classify and plot a barchart of the classifications
+#'
+#' @param spectra_matches DataFrame resulting from either Spec2Vec or MS2DeepScore. Need to feature smiles or inchi, score and compound_name as columns.
+#' @param color Color of the displayed bars
+
+
+#' @return A Barplot displaying the best matches
+#' @export
+plot_classyfire_result <- function(spectra_matches, color){
+  omigami <- reticulate::import("omigami")
+
+  plotter <- omigami$MoleculePlotter()
+  results <- plotter$plot_classyfire_result(spectra_matches = spectra_matches,
+                                            color = color)
+
+  return(results)
+}
+
+#' Uses the NP-Classifier API to classify natural products.
+#'
+#' @param spectra_matches DataFrame resulting from either Spec2Vec or MS2DeepScore. Need to feature smiles or inchi, score and compound_name as columns.
+#' @param color Color of the displayed bars
+
+
+#' @return A Barplot displaying the best matches
+#' @export
+plot_NPclassifier_result <- function(spectra_matches, color){
+  omigami <- reticulate::import("omigami")
+
+  plotter <- omigami$MoleculePlotter()
+  results <- plotter$plot_NPclassifier_result(spectra_matches = spectra_matches,
+                                              color = color)
+
+  return(results)
+}
